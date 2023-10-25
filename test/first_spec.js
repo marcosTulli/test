40a56efbfee5d3a6e68290b779984a07346a65f3
Marcos Tulli <marcos.tulli@raona.com>
2023-10-25 13:53:13 -0300

    First Commit

---
+++ test/first_spec.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+describe("A feature", () => {
+	describe("A Scenario", () => { 
+		it("does Something expected",  () => { 
+			assert(true);
+		});
+	});
+});
+
